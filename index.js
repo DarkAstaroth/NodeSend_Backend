6a50b38e002c0bfb3c4b53f600d5870e02523b81
Manuel <mninahuanca4@gmail.com>
2021-05-15 18:20:08 -0400

    Routing para usuarios

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ console.log('Comenzando Node Send');
 // Puerto de la app
 const port = process.env.PORT || 4000;
 
+// rutas de la app
+app.use('/api/usuarios', require('./router/usuarios'));
+
 // Arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
