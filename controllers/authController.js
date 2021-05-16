134e8699dc9d7411d54bb75366481a0b757d31a5
Manuel <mninahuanca4@gmail.com>
2021-05-16 10:15:42 -0400

    Revisando si el password es correcto o no

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,11 @@
 const Usuario = require('../models/Usuario');
+const bcrypt = require('bcrypt');
 
 exports.autenticarUsuario = async (req,res,next) => {
     // Revisar si hay errores
 
     // Buscar el usuario para ver si esta registrado
-    const { email } = req.body;
+    const { email, password} = req.body;
     const usuario = await Usuario.findOne({ email });
     
     if (!usuario) {
@@ -13,8 +14,13 @@ exports.autenticarUsuario = async (req,res,next) => {
     }
 
     //Verificar el password y autenticar el usuario
+    if (bcrypt.compareSync(password, usuario.password)) {
 
-    
+        //Crear JWT
+        
+    } else {
+        res.status(401).json({ msg: 'Password incorrecto' });
+    }
 }
 
 exports.usuarioAutenticado = (req, res) => {
