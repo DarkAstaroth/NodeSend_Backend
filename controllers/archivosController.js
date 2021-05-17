8139cb5916f4cf3a269cb5e443ebc8fb5497509d
Manuel <mninahuanca4@gmail.com>
2021-05-17 00:24:10 -0400

    creando una configuracion mas avanzada con multer

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,7 +1,31 @@
+const multer = require('multer');
+const shorid = require('shortid');
 
+const configuracionMulter = {
+    limits : { fileSize : 1024 * 1024 },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null,__dirname+'/../uploads')
+        },
+        filename: (req, file, cb) => {
+            const extension = file.mimetype.split('/')[1];
+            cb(null,`${shorid.generate()}.${extension}`);
+        }
+    })
+}
 
-exports.subirArchivo = async (req, res) => {
-    console.log(req.file);
+const upload = multer(configuracionMulter).single('archivo');
+
+exports.subirArchivo = async (req, res,next) => {
+    upload(req, res, async (error) => {
+        console.log(req.file);
+        if (!error) {
+            res.json({ archivo: req.file.filename });
+        } else {
+            console.log(error);
+            return next();
+        }
+    });
 }
 
 exports.eliminarArchivo = async (req, res) => {
